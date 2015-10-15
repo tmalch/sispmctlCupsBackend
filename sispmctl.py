5edf2ef0ced324506e40c25d1c70e24bb186dd5d
Thomas Malcher <malcher@student.tugraz.at>
2015-10-15 21:36:50 +0200

    cleaned a bit translated error messages and comments to english

--- sispmctl.py
+++ sispmctl.py
@@ -12,114 +12,97 @@ import time
 import signal
 import atexit
 
-def writelog(message):
-    #syslog.syslog(syslog.LOG_ALERT, message)
-    sys.stderr.write(message)
-
-def switchon(nr):
-    command_on = ["/usr/bin/sispmctl","-o",str(nr)]
-    subprocess.check_call(command_on)
-    writelog("INFO NR "+str(nr)+" einschaltet")
-
-def turnOFFproc(nr):
-    subprocess.Popen(['/usr/lib/cups/backend/sispmctl_turnoff.py',str(nr)])
+def error(message):
+    #syslog.syslog(syslog.LOG_ERR, message)
+    sys.stderr.write("ALERT: "+message+"\n")
+def debug(message):
+    #syslog.syslog(syslog.LOG_DEBUG, message)
+    sys.stderr.write("DEBUG: "+message+"\n")
 
 argc = len(sys.argv)
 pidfile = "/run/sispmctl_turnoff.pid"
 atexit.register(syslog.closelog)
 syslog.openlog('sispmctl_printer', syslog.LOG_PID, syslog.LOG_LPR)
 
-writelog("INFO: switchprinter argv[%s] = '%s'\n" % (argc, "', '".join(sys.argv)))
+debug("sispmctl argv[%s] = '%s'" % (argc, "', '".join(sys.argv)))
 
 if argc == 1:
     print "direct sispmctl \"Unknown\" \"SISPMCTL\""
     sys.exit(0)
 
 if argc < 6 or argc > 7:
-    writelog("ERROR: %s job-id user title copies options [file]\n" % sys.argv[0])
+    error("ERROR: %s job-id user title copies options [file]" % sys.argv[0])
     sys.exit(1)
 
 jobid = sys.argv[1]
 user = sys.argv[2]
 title = sys.argv[3]
 copies = sys.argv[4]
-if title[:7] == "smbprn.":
-    title = title[16:]
-opts = sys.argv[5].split(" ")
+opts = sys.argv[5]
+
 deviceuri = os.environ['DEVICE_URI']
-#deviceuri = "sispmctl://4/usb://Brother/HL-2030%20series"
+debug("INFO: Deviceuri:" + deviceuri)
 try:
 	if argc == 7:
-		  writelog("INFO: file is %s\n" % sys.argv[6])
 		  infilename = sys.argv[6]
+		  debug("INFO: file is "+infilename)
 	else:
-		  infilename = os.environ['TMPDIR']+"/"+user+jobid+".tmp"
-		  try:
-		      infile = open(infilename, "w")
-		  except:
-		      writelog("ERROR: unable to create tmp file %s\n" % infilename)
-		      sys.exit(1)
-		  writelog("INFO: file is stdin\n")
-		  try:
-		      infile.write(sys.stdin.read())
-		  except:
-		      writelog("ERROR: unable to copy into tmpfile\n")
-		      sys.exit(1)
-		  infile.close()
-		  writelog("INFO: copied stdin to %s\n" % infilename)
-
-	writelog("INFO: Deviceuri %s\n" % deviceuri)
-	##Parameter auslesen entweder aus URI oder Option (URI hat Vorang)
+			infilename = os.tempnam()
+			with open(infilename, "w") as infile:
+				infile.write(sys.stdin.read())
+			debug("INFO: copied stdin to tmp file " + infilename)
+	#decode outlet number
 	switchnr = None
-	if deviceuri[:11] == "sispmctl://":
+	if type(deviceuri) == str and deviceuri.startswith("sispmctl://"):
 		  nested_uri_idx = deviceuri.find("/", 11)
 		  if nested_uri_idx < 0:
-		    writelog("ERROR: falsche URI %s\n" % deviceuri)
-		    sys.exit(2)
+		    error("wrong URI format, could not find nested original URI "+deviceuri)
+		    sys.exit(1)
 		  switchnr = str(int(deviceuri[11:nested_uri_idx]))
 		  deviceuri = deviceuri[nested_uri_idx+1:]
-		  writelog("INFO: deviceuri %s\n" % deviceuri)
+		  debug("decoded original deviceuri " + deviceuri)
 	else:
-		  writelog("INFO: suche in options\n")
-		  for opt in opts:
+		  debug("search for number of outlet in options")
+		  for opt in opts.split(" "):
 		    if opt[:9] == "switchnr=":
 			  	switchnr = str(int(opt[9:]))
-			  	writelog("INFO: SwitchNr %d\n" % switchnr)
-
+			  	debug("switch outlet number " + switchnr)
+	if not switchnr:
+		error("was not able to decode outlet number")
+		sys.exit(1)
+	#check if turnoff process is already running, if so stop it 
 	if os.path.exists(pidfile):
-	    writelog("INFO TURNOFF Exists --> stop\n")
-	    try:
-	      tpid = open(pidfile,"r").readline(10)
-	      os.kill(int(tpid), signal.SIGTERM)
-	    except Exception as e:
-	      os.remove(pidfile)
-	      writelog("ERROR konnte turnoff nicht beenden"+str(e))
-	#if it is off:
-	switchon(switchnr)
+		debug("turnoff process already running -> stop it")
+		try:
+			with open(pidfile,"r") as f:
+				tpid = f.readline(10)
+			os.kill(int(tpid), signal.SIGTERM)
+		except Exception as e:
+			os.remove(pidfile)
+			error("ERROR was not able to kill turnoff process: "+str(e))
+	#turn the outlet for the printer on
+	subprocess.check_call(["/usr/bin/sispmctl","-o",str(switchnr)])
+	debug("outlet number "+str(switchnr)+" switched on")
 	time.sleep(10)
-
-
+	#call the original backend
 	i = deviceuri.find(":")
 	backend = "/usr/lib/cups/backend/"+deviceuri[:i]
-	writelog("ERROR: backend %s\n" % backend)
+	debug("orig backend " + backend)
 	os.environ['DEVICE_URI'] = deviceuri
-
 	title = "\""+title+"\""
-	options = "\""+" ".join(opts[0:])+"\""
-	command = [backend,jobid,user,title,copies,options,infilename]
-	writelog("INFO Exec backend %s\n" % " ".join(command[0:]))
-
-	p = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	p.wait()
-	
-	writelog("INFO: usb backend ist fertig.\n")
-	turnOFFproc(switchnr)
-	writelog("INFO: turnoff started.\n")
-
+	options = "\""+opts+"\""
+	orig_backend_command = [backend,jobid,user,title,copies,options,infilename]
+	debug("execute backend %s" % " ".join(command))
+	subprocess.check_call(command)
+	debug("usb backend ist fertig")
+	#start turnoff process
+	subprocess.check_call(['/usr/lib/cups/backend/sispmctl_turnoff.py',str(switchnr)])
+	debug("turnoff started")
+	sys.exit(0)
 except Exception as e:
-	writelog(str(e)+"\n")
+	error(str(e))
+	sys.exit(1)
 
-sys.exit(0)
 
 
 
--- sispmctl_turnoff.py
+++ sispmctl_turnoff.py
@@ -4,61 +4,54 @@
 import sys, os, syslog, subprocess
 import time
 import atexit
-import signal
-
+import daemon
 pidfile = "/run/sispmctl_turnoff.pid"
 
-def writelog(message):
-    syslog.syslog(syslog.LOG_ALERT, message)
-    sys.stderr.write(message)
-
-def switchoff(nr):
-    command_off = ["/usr/bin/sispmctl","-f",nr]
-    try:
-      subprocess.check_call(command_off)
-    except:
-      writelog("ERROR konnte NR %s nicht ausschalten" % nr)
+def error(message):
+    syslog.syslog(syslog.LOG_ERR, message)
+    sys.stderr.write("ALERT: "+message+"\n")
+def debug(message):
+    syslog.syslog(syslog.LOG_DEBUG, message)
+    sys.stderr.write("DEBUG: "+message+"\n")
 
 def delpidfile(pidfile):
     import os
     if os.path.exists(pidfile):
       os.remove(pidfile)
-      print("INFO PID file gelöscht\n")
+      print("PID file deleted")
 
 def createpidfile(pidfile):
     pid = str(os.getpid())
     f = file(pidfile,"w+")
     f.write("%s\n" % pid)
     f.close()
-    writelog("INFO PID file erstellt\n")
+    debug("PID file created")
 
-
-import daemon
 with daemon.DaemonContext():
-	atexit.register(syslog.closelog)
-	syslog.openlog('sispmctl_printer', syslog.LOG_PID, syslog.LOG_LPR)
 	try:
-		switchnr = sys.argv[1]
-		if switchnr.isdigit() == False:
-				writelog("ERROR 1. Argument ist keine Nummer")
+		atexit.register(syslog.closelog)
+		syslog.openlog('sispmctl_turnoff', syslog.LOG_PID, syslog.LOG_LPR)
+		if os.path.exists(pidfile):
+				error("turnoff process already running -> exit")
 				sys.exit(1)
 
-		if os.path.exists(pidfile):
-				writelog("ERROR turnoff läuft bereits")
+		switchnr = sys.argv[1]
+		if switchnr.isdigit() == False:
+				error("first argument has to be a number")
 				sys.exit(1)
 
 		atexit.register(delpidfile,pidfile)
 		createpidfile(pidfile)
-		writelog("INFO PID file created")
+		debug("PID file created")
 
 		time.sleep(5*60) # wait for 5 minutes till switching off 
 
-		writelog("INFO: Wartezeit vorbei switch off")
-		switchoff(switchnr)
-		writelog("INFO: switched off FERTIG")
-
+		debug("waiting time done, going to switch off outlet "+switchnr)
+		command_off = ["/usr/bin/sispmctl","-f",nr]
+		subprocess.check_call(command_off)
+		debug("switched off outlet "+switchnr)
 		sys.exit(0)
 	except Exception as e:
-		writelog("ERROR: "+str(e))
+		error("ERROR: "+str(e))
 
 
